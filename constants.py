db8f629d3d1ae24f02451455d6586da1ab66bb87
Macan Mehri <macanmehri@gmail.com>
2024-03-22 22:56:40 +0330

    Created constants file to store constant variables in it

---
+++ constants.py
@@ -0,0 +1,7 @@
+# Urls
+BASE_URL = 'https://www.goodreads.com/'
+SEARCH_URL = BASE_URL + 'search?page={page_number}&q={search_title}&search_type={search_type}&tab={tab}'
+# Default values
+DEFAULT_TAB = 'books'
+DEFAULT_PAGE_COUNTS = 5
+DEFAULT_TITLE = 'book'
