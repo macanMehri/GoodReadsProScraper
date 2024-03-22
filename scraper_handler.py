e57d7e001f5311e119be76383b2a9b285778c3ff
Macan Mehri <macanmehri@gmail.com>
2024-03-22 22:59:06 +0330

    Created a scraper handler to help scraping and added a function to scrape data of authers

---
+++ scraper_handler.py
@@ -0,0 +1,53 @@
+import requests
+from bs4 import BeautifulSoup
+
+
+class ScraperHandler:
+    """A class to handle scraping better"""
+
+    def __init__(self, base_url):
+        self.base_url = base_url
+        self.response = None
+        self.soup = None
+
+    def send_request(self) -> requests.Response:
+        """Send a request to the base url"""
+        self.response = requests.get(url=self.base_url, timeout=20)
+        # If request sent successfully then create a soup object
+        if self.status_code == 200:
+            self.soup = BeautifulSoup(self.html_source_code, 'html.parser')
+        return self.response
+
+    @property
+    def status_code(self) -> int:
+        """Returns response status code"""
+        return self.response.status_code
+
+    @property
+    def html_source_code(self) -> str:
+        """Returns html source code of base url"""
+        return self.response.text
+
+    def get_target_urls(self):
+        """Returns the url of target website to scrape"""
+        targets = self.soup.find_all('a', attrs={'class': 'bookTitle'})
+        for target in targets:
+            yield target['href']
+
+    def get_author_info(self):
+        """Scrape authors info and creates and returns an author object"""
+        name = self.soup.find('span', attrs={'class': 'ContributorLink__name'}).text
+        number_of_books = self.soup.find('div', attrs={'class': 'PageSection'}
+                                         ).find_next('span', attrs={'class': 'Text Text__body3 Text__subdued'}).text
+
+        number_of_books_end = number_of_books.find('b') - 1
+        # Remove number separator (,)
+        number_of_books = number_of_books.replace(',', '')
+        # Slice string and convert to int
+        number_of_books = int(number_of_books[:number_of_books_end])
+
+        number_of_followers = self.soup.find('div', attrs={'class': 'PageSection'}
+                                             ).find_next('span', attrs={'class': 'u-dot-before'}).text
+
+        print(f'name: {name}, books: {number_of_books}, followers: {number_of_followers}')
+
