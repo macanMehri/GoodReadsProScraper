9695413381875477e4416cdc6c3be73d00f9d99e
Macan Mehri <macanmehri@gmail.com>
2024-03-23 15:09:46 +0330

    Added item class dictionary to constants

--- constants.py
+++ constants.py
@@ -5,3 +5,11 @@ SEARCH_URL = BASE_URL + 'search?page={page_number}&q={search_title}&search_type=
 DEFAULT_TAB = 'books'
 DEFAULT_PAGE_COUNTS = 5
 DEFAULT_TITLE = 'book'
+
+ITEM_CLASS = {
+    'books': 'bookTitle',
+    'groups': 'groupName',
+    'quotes': 'authorOrTitle',
+    'people': '',
+    'lists': 'listTitle'
+}
