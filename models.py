3ea01a24763efc5f152a3a37c7327c33b21f88aa
Macan Mehri <macanmehri@gmail.com>
2024-03-23 15:33:10 +0330

    Removed unused classes

--- models.py
+++ models.py
@@ -37,26 +37,11 @@ class Author(BaseModel):
         return f'{self.name} followed by {self.followers} and written {self.books} books'
 
 
-class Edition(BaseModel):
-    edition_format = peewee.CharField(max_length=255, null=False, verbose_name='Edition Format')
-    edition_published_date = peewee.CharField(max_length=255, null=False, verbose_name='Edition Published Date')
-    isbn = peewee.CharField(max_length=255, null=False, verbose_name='ISBN')
-    language = peewee.CharField(max_length=255, null=False, verbose_name='Language')
-
-    class Meta:
-        database = main.database_manager.db
-
-    def __str__(self) -> str:
-        """Override str method"""
-        return f'{self.isbn}'
-
-
 class Book(BaseModel):
     url = peewee.TextField(null=False, verbose_name='Link')
     title = peewee.CharField(max_length=255, null=False, verbose_name='Title')
     author = peewee.ForeignKeyField(model=Author, null=False)
     rate = peewee.CharField(max_length=255, null=False, verbose_name='Rate')
-    first_published_date = peewee.CharField(max_length=255, null=False, verbose_name='First Published Date')
 
     class Meta:
         database = main.database_manager.db
